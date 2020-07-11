0f827ad6910d1a73ab37bba5a58e5508493852c8
Leo <Leo>
2020-07-11 15:14:17 -0600

    First Comit

---
+++ TestConexus/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestConexus.Models;
+
+namespace TestConexus.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Person> personList;
+        public ActionResult Index()
+        {
+            GetJsonData();
+           
+            return View();
+        }
+
+        private void GetJsonData()
+        {
+            var json = System.IO.File.ReadAllText(Server.MapPath(@"~/json/Datos_Prueba_Dev.json"));
+            personList = JsonConvert.DeserializeObject<List<Person>>(json);
+
+            ViewBag.personList = personList;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
